44e0e7f38ad790f8e9dc84c431625e130f195cc8
unknown <tyrome.douglass@gmail.com>
2019-09-18 15:26:57 -0600

    problem2

--- practice.js
+++ practice.js
@@ -16,6 +16,10 @@ var arr = [10,20,30];
   Create a function named 'first' that is given 'arr' as an argument.
   Return the first item in the given array.
 */
+function first(arr){
+  return(arr[0])
+}
+first(arr)
 
 //Code Here
 
@@ -31,7 +35,9 @@ var arr = [40,50,60];
   Create a function named 'last' that is given 'arr' as an argument. 
   Return the last item in the given array.
 */
-
+function last(arr){
+  return arr.pop()
+}
 //Code Here
 
 
@@ -47,8 +53,14 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
 
+
+//Code Here
+function looper (family){
+  for(i = 0; i < family.length; i ++){
+alert(family[i])
+}
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,8 +75,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
-
+function reversedLooper(letters){
+  for (i = letters.length - 1; i >= 0; i--){
+    alert(letters[i])
+    console.log(letters[i])
+  }
+}
+reversedLooper(letters)
 
 ////////// PROBLEM 5 //////////
 
@@ -79,10 +96,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
-
-
-
+function evenFinder(nums){
+  let num1 = []
+  for(i = 0; i < nums.length; i++){
+    if (nums[i] % 2 === 0){
+      num1.push(nums[i])
+    }
+  }
+  return num1
+}
 
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
